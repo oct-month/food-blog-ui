f667fbc845c3f6b82f28732ff0e594a31cc7d9e0
ablocker <oct-month@qq.com>
2020-12-06 21:11:13 +0800

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="about">
-    <h1>美食博客</h1>
+    <h1>不只是美食</h1>
+    <div>
+      <p>1、任何一种爱，都不比对美食的热爱真切。——萧伯纳 George Bernard Shaw</p>
+
+      <p>2、这世界如果有更多的人热爱美食与诗歌胜过爱黄金，这世界会是一个更美好的地方。——托尔金 J.R.R.Tolkien</p>
+
+      <p>3、只要告诉我你爱吃什么，我就能知道你是什么样的人。——萨瓦林 Jean Anthelme Brillat-Savarin</p>
+
+      <p>4、红酒是瓶装的诗歌。——罗伯特•刘易斯史蒂文森 Robert Louis Stevenson</p>
+
+      <p>5、别吃任何你曾祖母看不出来是食物的食物。——迈克尔•波伦 Michael Pollan</p>
+
+      <p>6、我厌恶那些对美食不认真的人，他们都是肤浅的。——奥斯卡•王尔德 Oscar Wilde</p>
+
+      <p>7、美食是身体的歌曲；而歌曲是心灵的美食。名人名言  www.51Flash.Com——格里高利•罗伯兹 Gregory David Roberts</p>
+
+      <p>8、我们经常听到新闻说，“有人被鲨鱼咬死”；但不曾听过有人从另一观点看并且说，“有人忘了自己是鲨鱼的美食”。——Gary Larson</p>
+
+      <p>9、我们对小时候吃的美食都有着深刻记忆……美食有着带我们回到过去的魔力。——霍马洛•坎图 Homaro Cantu</p>
+
+      <p>10、满足身体的美食是不足够的，也得要有满足心灵的美食。——陶乐斯•戴 Dorothy Day</p>
+    </div>
   </div>
 </template>
 
